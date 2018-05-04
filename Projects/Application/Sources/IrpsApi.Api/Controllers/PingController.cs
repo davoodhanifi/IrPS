fc11ccf365da8988142b0a84a29a4390fb9c27e7
Davood Hanifi <davood.hanifi@gmail.com>
2018-05-04 21:27:20 +0430

    #4 - Added ping method.

---
+++ Projects/Application/Sources/IrpsApi.Api/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IrpsApi.Api.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public void TestConnection()
+        {
+        }
+    }
+}
